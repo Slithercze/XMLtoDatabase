07c843b8921cee4850e4fef26c72fe371bc57d61
Miroslav Doan <54637695+Slithercze@users.noreply.github.com>
2021-12-02 18:22:32 +0100

    Add files via upload

---
+++ XMLtoDB/demo/src/main/java/com/example/XMLtoDB.java
@@ -0,0 +1,14 @@
+package com.example;
+
+/**
+ * XMLtoDB
+ */
+public class XMLtoDB {
+
+    public static void main(String[] args) {
+        CreateDB.createDB();
+        Obec.obec();
+        CastObce.castObce();
+        
+    }
+}
